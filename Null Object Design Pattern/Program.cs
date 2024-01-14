c94b8669b644589bbaeca91bcf1ecf7a31bb6cba
shubham <shubham.m@carwale.com>
2024-01-14 20:07:07 +0530

    added null design pattern

---
+++ Null Object Design Pattern/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Null_Object_Design_Pattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GetVehicleSpecs(new Null());
+    }
+    private static void GetVehicleSpecs(Vehicle vehicle)
+    {
+        // if(vehicle != null) : Now you can avoid writing this line for checking null after adding null design pattern
+        // for vehicle object
+        Console.WriteLine(vehicle.GetTankingCapacity());
+        Console.WriteLine(vehicle.GetSeatingCapacity());
+    }
+}
---
+++ Null Object Design Pattern/Vehicle.cs
@@ -0,0 +1,8 @@
+namespace Null_Object_Design_Pattern
+{
+    public interface Vehicle
+    {
+        public int GetSeatingCapacity();
+        public int GetTankingCapacity();
+    }
+}
